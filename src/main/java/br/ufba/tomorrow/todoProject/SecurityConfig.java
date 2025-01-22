141cb1eb00c46e832f48a4f12e532008897ce4b3
Gabriel <gabrielleal@infojr.com.br>
2025-01-22 10:18:16 -0300

    Security

--- src/main/java/br/ufba/tomorrow/todoProject/SecurityConfig.java
+++ src/main/java/br/ufba/tomorrow/todoProject/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder
@@ -63,7 +63,8 @@ public class SecurityConfig {
         return http.build();
     }
 
-    private CorsConfigurationSource corsConfigurationSource() {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.addAllowedOrigin("*");
         corsConfig.addAllowedMethod("*");
